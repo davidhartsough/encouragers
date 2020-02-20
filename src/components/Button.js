853699988228c99b0a1ee06eaed101482b1fba67
Toaster Forks <wibbels.leoo@gmail.com>
2020-02-20 16:52:10 -0700

    added Create Group Page.

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button({ label, onClick }) {
+    return (
+        <div>
+            <button onClick={onClick}>{label}</button>
+        </div>
+    )
+}
--- src/routes/groups/NewGroup.js
+++ src/routes/groups/NewGroup.js
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select"
+import Async from 'react-select/async'
+import FormField from "../../components/FormField";
+import Button from "../../components/Button";
+import PageLoader from "../../components/PageLoader";
+import Header from "../../components/Header";
+
+const promiseOptions = inputValue =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve([{ value: 'steve123', label: 'Steve' },]);
+    }, 1000);
+  });
+
 
 export default function NewGroup() {
-  return <div>NewGroup</div>;
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState("");
+  const [members, setMembers] = useState([]);
+  const [admins, setAdmins] = useState([]);
+  const proceed = () => setStep(step + 1);
+  function finish() {
+    console.log(name, members, admins);
+  }
+  if (step === 1) {
+    return (
+      <div>
+        <Header title="Create a group" />
+        <FormField label="Name" placeholder="Group Name we will make it more spicy later you know" value={name} setValue={setName} />
+        <Button label="Next" onClick={proceed} />
+      </div>
+    );
+  } else if (step === 2) {
+    return (
+      <div>
+        <Header title="Add members" />
+        <p>Pick people to join your group</p>
+        <Async isMulti cacheOptions loadOptions={promiseOptions} values={members} onChange={setMembers} />
+        <Button label="Next" onClick={proceed} />
+      </div>
+    )
+  } else if (step === 3) {
+    return (
+      <div>
+        <Header title="Choose admins" />
+        <p>Who else should be an admin?</p>
+        <Select
+          isMulti
+          options={members}
+          values={admins}
+          onChange={setAdmins}
+        />
+        <Button label="Finish" onClick={finish} />
+      </div>
+    );
+  } else {
+    return <PageLoader />;
+  }
 }
